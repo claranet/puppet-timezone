5a58e408619c69a05bb89b1e9edb60adcaf8f4bf
Craig Watson <craig.watson@claranet.uk>
2018-08-30 09:33:23 +0100

    PDK: Adding managed files

--- spec/classes/timezone_spec.rb
+++ spec/classes/timezone_spec.rb
@@ -1,12 +1,9 @@
 require 'spec_helper'
 
-describe 'timezone', :type => :class do
+describe 'timezone', type: :class do
   on_supported_os.each do |os, facts|
-
     let(:facts) do
-      facts.merge({
-        :path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/snap/bin:/opt/puppetlabs/bin',
-      })
+      facts.merge(path: '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/snap/bin:/opt/puppetlabs/bin')
     end
 
     context "on #{os}" do
